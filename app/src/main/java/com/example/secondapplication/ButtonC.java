fefe357c966e4ecae488d59b3b290da0af5a987b
hyunOO <khw7147@kaist.ac.kr>
2017-07-10 22:02:54 +0900

    B탭 추가

--- app/src/main/java/com/example/secondapplication/ButtonC.java
+++ app/src/main/java/com/example/secondapplication/ButtonC.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.speech.RecognitionListener;
@@ -12,11 +13,13 @@ import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,7 +30,6 @@ import okhttp3.OkHttpClient;
  */
 
 public class ButtonC extends AppCompatActivity {
-    OkHttpClient client = new OkHttpClient();
     Intent intent;
     SpeechRecognizer mRecognizer;
     TextView txv;
@@ -36,19 +38,17 @@ public class ButtonC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_c);
-
         ImageButton ib = (ImageButton) findViewById(R.id.mike);
         txv = (TextView) findViewById(R.id.problem);
-        final Context mContext = ButtonC.this;
-        final String str;
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(listener);
+
         ib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
-                mRecognizer = SpeechRecognizer.createSpeechRecognizer(mContext);
-                mRecognizer.setRecognitionListener(listener);
                 mRecognizer.startListening(intent);
             }
         });
@@ -56,34 +56,49 @@ public class ButtonC extends AppCompatActivity {
     private RecognitionListener listener = new RecognitionListener() {
         @Override
         public void onReadyForSpeech(Bundle params) {
-
         }
-
         @Override
         public void onBeginningOfSpeech() {
-
         }
-
         @Override
         public void onRmsChanged(float rmsdB) {
-
         }
-
         @Override
         public void onBufferReceived(byte[] buffer) {
-
         }
-
         @Override
         public void onEndOfSpeech() {
-
         }
-
         @Override
         public void onError(int error) {
+            if(error == mRecognizer.ERROR_NETWORK_TIMEOUT){
+                Toast.makeText(getApplicationContext(),"네트워크 타임아웃 에러",Toast.LENGTH_SHORT).show();
+            }
+            else if(error == mRecognizer.ERROR_NETWORK){
+                Toast.makeText(getApplicationContext(),"네트워크 에러",Toast.LENGTH_SHORT).show();
+            }
 
+            else if(error == mRecognizer.ERROR_AUDIO){
+                Toast.makeText(getApplicationContext(),"녹음 에러",Toast.LENGTH_SHORT).show();
+            }
+
+            else if(error == mRecognizer.ERROR_SERVER){
+                Toast.makeText(getApplicationContext(),"서버 에러",Toast.LENGTH_SHORT).show();
+            }
+
+            else if(error == mRecognizer.ERROR_CLIENT){
+                Toast.makeText(getApplicationContext(),"클라이언트 에러",Toast.LENGTH_SHORT).show();
+            }
+            else if(error == mRecognizer.ERROR_SPEECH_TIMEOUT){
+                Toast.makeText(getApplicationContext(),"아무 음성도 듣지 못함",Toast.LENGTH_SHORT).show();
+            }
+            else if(error == mRecognizer.ERROR_NO_MATCH){
+                Toast.makeText(getApplicationContext(),"적당한 결과를 찾지 못함",Toast.LENGTH_SHORT).show();
+            }
+            else if(error == mRecognizer.ERROR_RECOGNIZER_BUSY){
+                Toast.makeText(getApplicationContext(),"인스턴스가 바쁨",Toast.LENGTH_SHORT).show();
+            }
         }
-
         @Override
         public void onResults(Bundle results) {
             String key = "";
@@ -92,19 +107,13 @@ public class ButtonC extends AppCompatActivity {
             String[] rs = new String[mResult.size()];
             mResult.toArray(rs);
             txv.setText(""+rs[0]);
-            mRecognizer.startListening(intent);
         }
-
         @Override
         public void onPartialResults(Bundle partialResults) {
-
         }
-
         @Override
         public void onEvent(int eventType, Bundle params) {
-
         }
     };
-
 }
 
